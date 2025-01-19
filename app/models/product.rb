acc01ebaad32e03bf9f5fffe873be27632b35662
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-01-19 17:05:54 +0200

    Notify subscribers when product is back in stock

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,16 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
+
+  after_update_commit :notify_subscribers, if: :back_in_stock?
+
+  def back_in_stock?
+    inventory_count.previously_was.zero? && inventory_count > 0
+  end
+
+  def notify_subscribers
+    subscribers.each do |subscriber|
+      ProductMailer.with(product: self, subscriber:).in_stock.deliver_later
+    end
+  end
 end
